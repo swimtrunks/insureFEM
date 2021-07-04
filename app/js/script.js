01b9a3c2316aa8fce2a276cc2fbd1f2cf9c093c3
Malcolm Peterson <mgpetersn@gmail.com>
2021-07-04 15:41:09 -0400

    sunday test1
    added fixed nav bar on desktop and mobile. starting link overlay on mobile and working on animated hamburger menu on mobile instead of just using images.

--- app/js/script.js
+++ app/js/script.js
@@ -1,2 +1,8 @@
 'use strict'
 
+$(document).ready(function () {
+    $('#nav-icon4').click(function () {
+        $(this).toggleClass('open'); 
+        console.log(document.getElementById('nav-icon4')) 
+    });
+});
